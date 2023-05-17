3fb1ab43b5815fe15c0cfd57342d16bab7a4b00e
1084350607 <1084350607@qq.com>
2023-05-17 10:37:04 +0800

    feat: add textobj & fix telescope hidden

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,13 +3,24 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
 		cmd = "Telescope",
 		keys = {
-			{ "<Leader>f", ":Telescope find_files<CR>", {} },
+			{ "<Leader>f", "<cmd>Telescope find_files hidden=true<CR>", {} },
 			{ "<Leader>p", ":Telescope live_grep<CR>", {} },
 			{ "<Leader>o", ":Telescope lsp_document_symbols<CR>", {} },
+			{ "<Leader>rs", ":Telescope resume<CR>", {} },
 			{ "<C-q>", ":Telescope oldfiles<CR>", {} },
 		},
 		config = function ()
 			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {"node_modules"}
+				},
+				pickers = {
+					live_grep = {
+						additional_args = function (opt)
+							return {"--hidden"}
+						end
+					},
+				},
 			})
 		end,
 }
--- lua/plugins/textobj.lua
+++ lua/plugins/textobj.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"xiantang/nvim-treesitter-textobjects",
+		"nvim-treesitter/nvim-treesitter-textobjects",
 		event = "VeryLazy",
 		config = function()
 			require("nvim-treesitter.configs").setup({
