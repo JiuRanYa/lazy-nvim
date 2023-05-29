1f2cd74176f386d6e87b76671c96c358a04daf16
1084350607 <1084350607@qq.com>
2023-05-29 20:37:54 +0800

    feat: support openin finder

--- lua/config/vimc.lua
+++ lua/config/vimc.lua
@@ -5,12 +5,19 @@ vim.opt.foldlevel = 99
 -- treesitter illuminate
 vim.cmd("hi IlluminatedWordRead guibg=#525252 gui=none")
 
-vim.cmd([[
-  command! OpenInFinder execute "!open " . shellescape(expand("%:p:h"))
-]])
-
-vim.g.vsnip_filetypes = {
-  typescriptreact = {"javascript", 'typescript', 'typescriptreact'},
-	typescript = {"javascript", 'typescript', 'typescriptreact'}
-}
-
+-- vim.cmd([[
+--   command! OpenInFinder execute "!open " . shellescape(expand("%:p:h"))
+-- ]])
+--
+-- vim.g.vsnip_filetypes = {
+--   typescriptreact = {"javascript", 'typescript', 'typescriptreact'},
+-- 	typescript = {"javascript", 'typescript', 'typescriptreact'}
+-- }
+--
+vim.api.nvim_create_user_command('OpenInFinder',
+	function()
+			local path = vim.api.nvim_buf_get_name(0)
+			os.execute('open -R ' .. path)
+	end,
+	{}
+)
