e43a6747569f2f2e9a5a297cb896dc69bb7f8fce
1084350607 <1084350607@qq.com>
2023-08-15 11:25:56 +0800

    feat: support open in finder

--- lua/config/vimc.lua
+++ lua/config/vimc.lua
@@ -14,10 +14,16 @@ vim.g.vsnip_filetypes = {
 	typescript = {"javascript", 'typescript', 'typescriptreact'}
 }
 --
-vim.api.nvim_create_user_command('OpenInFinder',
-	function()
-			local path = vim.api.nvim_buf_get_name(0)
-			os.execute('open -R ' .. path)
-	end,
-	{}
-)
+function open_folder_in_finder()
+	-- 获取 nvim-tree 插件的 API
+	local api = require "nvim-tree.api"
+	local node = api.tree.get_node_under_cursor()
+
+	local filepath = node.absolute_path
+	local escaped_path = vim.fn.shellescape(filepath)
+	local command = string.format('open -R %s', escaped_path)
+	vim.fn.system(command)
+end
+
+-- 将该函数映射到一个合适的按键，例如 F2
+vim.api.nvim_set_keymap('n', '<leader>of', ':lua open_folder_in_finder()<CR>', { noremap = true, silent = true })
