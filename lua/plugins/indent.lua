1e027f2c32fa82cb591dfbf637801a5581b896d6
Tong Siyuan <jiuran@JiuRandeMacBook-Pro.local>
2023-11-29 11:12:59 +0800

    fix: indent to version 3

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,14 +1,6 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			vim.opt.list = true
-			vim.opt.listchars:append("eol:↲")
-			require("indent_blankline").setup({
-				show_end_of_line = true,
-			})
-		end,
-		event = "VeryLazy",
+	{ 
+		"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {},
 	},
 	{
 		"echasnovski/mini.indentscope",
